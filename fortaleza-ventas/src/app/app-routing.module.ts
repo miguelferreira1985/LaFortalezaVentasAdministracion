ff9df9520814bfc6174f1ac809e845239afaf66e
Miguel Ferreira <miguel.fermo1985@gmail.com>
2021-01-05 14:03:25 -0600

    Login UI ready according with the design, styles file created

--- fortaleza-ventas/src/app/app-routing.module.ts
+++ fortaleza-ventas/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
