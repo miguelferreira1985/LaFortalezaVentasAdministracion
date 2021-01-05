9fecc0f365c1029367dc24686b12d9b858606de4
Miguel Angel Ferreira Moreno <miguel.fermo1985@gmail.com>
2021-01-05 14:11:01 -0600

    Revert "Login UI ready according with the design, styles file created"

--- fortaleza-ventas/src/app/app-routing.module.ts
+++ fortaleza-ventas/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
